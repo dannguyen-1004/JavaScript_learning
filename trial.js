59125623faf99ad3a9d90d01068adb26da6e0594
dannguyen-1004 <dmnguyen@bluevalleyk12.net>
2021-04-10 19:51:20 -0500

    Switch Case Statement

---
+++ trial.js
@@ -0,0 +1,18 @@
+var day = 5;
+switch(day){
+    case 1:
+        document.write("Sunday ");
+        break;
+    case 2:
+        document.write("Monday ");
+    case 3:
+         document.write("Tuesday ");
+        break;
+    case 4:
+        document.write("Wednesday ");
+        break;
+    default:
+        document.write("Wrong input");
+}
+
+
