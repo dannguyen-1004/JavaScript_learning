48b16dcc380add053f36e42182192cc8758ac05a
dannguyen-1004 <dmnguyen@bluevalleyk12.net>
2021-04-11 13:24:02 -0500

    Object Pt1

--- trial.js
+++ trial.js
@@ -1,12 +1,28 @@
 
-var output = add2Number(3,5);
-document.write(output);
+/*var car = {
+    car_brand:"Tesla",
+    car_model: "Model 3",
+    price: 35000,
 
-function add2Number(a,b)
+    teslaAutoPilot: function(){
+        document.write("This car has autopilot");
+    }
+
+    
+}*/
+//instead we can do create new object
+function Cars(car_brand,car_model,price)
 {
-    var total = a + b;
-    return total;
+    this.car_brand = car_brand;
+    this.car_model = car_model;
+    this.price = price;
+    this.tesLaAutoPilot = function()
+    {
+        document.write("This car has autopilot");
+    }
 }
 
-
-
+var c1 = new Cars("Tesla","Model", 35000);
+var c2 = new Cars("Honda", "Model P", 45000);
+c1.tesLaAutoPilot();
+document.write("<h2>" + c2.car_model);
