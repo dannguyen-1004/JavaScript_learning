9252fd020a229344232707d528ddf43b8bb70893
dannguyen-1004 <dmnguyen@bluevalleyk12.net>
2021-04-15 18:57:12 -0500

    DOM Manipulation

--- trial.js
+++ trial.js
@@ -1,33 +1,5 @@
-var car = {
-    brand: "Tesla",
-    model: "Model 3",
-    price: 35000,
-
-    teslaAutoPilot: function()
-    {
-        return "This car has autopilot";
-    }
-}
-//add properties
-car.oil = "$30";
-document.write(car.oil);
-
-//delete properties
-delete car.model;
-document.write( car.model);
-
-
-/*function Cars(brand, model, price)
+//DOM manipulation & getelementbyId method
+function buttonClick()
 {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-    this.teslaAutoPilot = function(){
-        document.write ("This car has autopilot");
-    }
+    document.getElementById("heading").innerHTML = "JavaScript Learning";
 }
-
-var c1 = new Cars("Tesla", "Model 3", 35000);
-document.write(c1.model);*/
-
-
