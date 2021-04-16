e30398bbca2641531b9afd218decfc39e7af9576
dannguyen-1004 <dmnguyen@bluevalleyk12.net>
2021-04-15 19:12:41 -0500

    Taking input from user

--- trial.js
+++ trial.js
@@ -1,5 +1,14 @@
 //DOM manipulation & getelementbyId method
-function buttonClick()
+function appear()
 {
-    document.getElementById("heading").innerHTML = "JavaScript Learning";
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username == "dmnguyen" & password == "10146645")
+    {
+        alert("Your user name and password is correct");
+    }
+    else
+    {
+        alert("Your user name or password is uncorrect. Please check!");
+    }
 }
