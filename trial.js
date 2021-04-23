b64ce93082985f1c1e42ae82fc08abda1611a7fa
dannguyen-1004 <dmnguyen@bluevalleyk12.net>
2021-04-23 15:53:42 -0500

    mouseover & mouseout event

--- trial.js
+++ trial.js
@@ -1,9 +1,8 @@
-//DOM manipulation
-function changeStyle()
-{
-    var element = document.getElementsByClassName("mypara");
-    for( var i = 0; i < element.length; i++)
-    {
-        element[i].style.color="red";
-    }
+//MOUSEOVER & MOUSE OUT EVENT
+
+function setNewImage(){
+    document.getElementById("img").src = "https://th.bing.com/th/id/R9f29389e1d15976ee9c1d563646b97ee?rik=eftC3AtllYK4dA&pid=ImgRaw";
+}
+function setOldImage(){
+    document.getElementById("img").src = "https://th.bing.com/th/id/Rf7f160c4d53748cea7e2a29074ffb85b?rik=MSNe%2bFdA0PbCuQ&pid=ImgRaw";
 }
